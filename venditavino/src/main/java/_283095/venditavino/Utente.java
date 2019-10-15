c307af1550f2dd7a1a41f0a2563aa69804b75eb5
Mattia <locastromattia@hotmail.it>
2019-10-15 14:23:40 +0200

    Added Status attribute in Person

--- venditavino/src/main/java/_283095/venditavino/Utente.java
+++ venditavino/src/main/java/_283095/venditavino/Utente.java
@@ -3,6 +3,6 @@ package _283095.venditavino;
 public class Utente extends Person {
 
 	public Utente(String _name, String _surname, String _email, String _pwd) {
-		super(_name, _surname, _email, _pwd);
+		super(_name, _surname, _email, _pwd, false);
 	}
 }
