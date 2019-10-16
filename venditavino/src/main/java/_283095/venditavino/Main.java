6bd1b8fc0b2546de0bbbb71a013f08d34442a303
Cosmin Gugoasa <cosmindaxgugoasa@gmail.com>
2019-10-16 18:15:17 +0200

    Added Inner Menu, Logout, Exit

--- venditavino/src/main/java/_283095/venditavino/Main.java
+++ venditavino/src/main/java/_283095/venditavino/Main.java
@@ -5,21 +5,10 @@ import java.util.Scanner;
 public class Main {
 
 	static Scanner keyboard = new Scanner(System.in);
-	static int menu_chose;
+	static Person loggedUser = null;
 
 	public static void main(String[] args) {
 		PrintMainMenu();
-		menu_chose = keyboard.nextInt();
-
-		switch (menu_chose) {
-		case 1:
-			PrintLoginMenu();
-			break;
-		case 2:
-			PrintRegisterMenu();
-			break;
-
-		}
 	}
 
 	static void PrintMainMenu() {
@@ -30,6 +19,22 @@ public class Main {
 		System.out.println("[1] Login");
 		System.out.println("[2] Register");
 		System.out.println("[3] Exit");
+
+		int _chose = keyboard.nextInt();
+
+		switch (_chose) {
+		case 1:
+			PrintLoginMenu();
+			PrintHomeMenu();
+			break;
+		case 2:
+			PrintRegisterMenu();
+			PrintHomeMenu();
+			break;
+		case 3:
+			System.exit(0);
+			break;
+		}
 	}
 
 	static void PrintLoginMenu() {
@@ -50,6 +55,9 @@ public class Main {
 		/*
 		 * Call Login Function with _email & _pwd
 		 */
+
+		// solo per test, da cancellare
+		loggedUser = new Utente("r", "r", "r", "r");
 	}
 
 	static void PrintRegisterMenu() {
@@ -75,11 +83,87 @@ public class Main {
 		_pwd = keyboard.next();
 
 		System.out.println(_name + _surname + _email + _pwd);
-		
+
 		// TODO
 		/*
 		 * Call Register Function with _email & _pwd
 		 */
+	}
+
+	static void PrintHomeMenu() {
+
+		// controllo che l'utente nel sistema non sia nullo
+		if (loggedUser == null) {
+			System.out.println("Login or Register User is null");
+			PrintMainMenu();
+		}
+
+		// stampo menu in base al tipo di utente che ha fatto login/reg
+		if (loggedUser instanceof Utente) {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("\n");
+			}
+			System.out.println("	MENU	");
+			System.out.println("[1] Search by name");
+			System.out.println("[2] Search by year");
+			System.out.println("[3] Logout");
+			System.out.println("[4] Exit");
+
+			int _choose = keyboard.nextInt();
+			switch (_choose) {
+			case 1:
+				System.out.println("Name : ");
+				String _name = keyboard.next();
+				/*
+				 * call search by name function
+				 */
+				System.out.println("Searched name : " + _name);
+				break;
+			case 2:
+				System.out.println("Year : ");
+				int _year = keyboard.nextInt();
+				/*
+				 * call search by year function
+				 */
+				System.out.println("Searched year : " + _year);
+				break;
+			case 3:
+				loggedUser = null;
+				PrintMainMenu();
+				break;
+			case 4:
+				System.exit(0);
+				break;
+			}
+
+		} else {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("\n");
+			}
+			System.out.println("	MENU	");
+			System.out.println("[1] Add wines to magazine");
+			System.out.println("[2] Process wine requests");
+			System.out.println("[3] Logout");
+			System.out.println("[4] Exit");
+
+			int _choose = keyboard.nextInt();
+
+			switch (_choose) {
+			case 1:
+				// call add wine function
+				break;
+			case 2:
+				// process request function
+				break;
+			case 3:
+				loggedUser = null;
+				PrintMainMenu();
+				break;
+			case 4:
+				System.exit(0);
+				break;
+			}
+		}
 	}
 
 }
